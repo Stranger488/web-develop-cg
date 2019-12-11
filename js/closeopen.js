61510456ed99d3570290c79e7834952c7c921b84
Aleksandr Kosenkov <Marveel@yandex.ru>
2019-12-11 11:21:35 +0300

    feat(style): add animation on info dropdown

--- js/closeopen.js
+++ js/closeopen.js
@@ -1,8 +1,20 @@
-function closeopen( id )
-    /*Для создания раскрывающихся по клику кнопочек*/
-    {
-     element = document.getElementById( id );
-          if( element )   
-             element.style.display = element.style.display == "none" ? "" : "none";   
+const classNames = {
+    infoDropdown: "info-dropdown",
+    dropdownActive: "info-dropdown_active",
+};
+
+function closeopen(id) {
+/*Для создания раскрывающихся по клику кнопочек*/
+    const element = document.getElementById(id);
+    if (element) {
+        // element.style.display = element.style.display === "none" ? "" : "none";
+        if (element.classList.contains(classNames.infoDropdown)) {
+            if (element.classList.contains(classNames.dropdownActive)) {
+                element.classList.remove(classNames.dropdownActive);
+            } else {
+                element.classList.add(classNames.dropdownActive);
+            }
+        }
+    }
 }
    
