03e78d46f61187329f301a0fc26fe76ae6732913
Aleksandr Kosenkov <Marveel@yandex.ru>
2019-12-25 13:09:37 +0300

    feat(vendor): replace links to local, fix()

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-
     if (window.innerWidth > 1100) {
         $('.dropdown').hover(function() {
             $('#overlay').css('background-color', 'rgba(0,0,0, 0.5)');
@@ -38,7 +37,7 @@ $( document ).ready(function() {
     }
 });
 
-$(window).scroll(function () {
+const scrollHeaderHandler = () => {
     if($(window).scrollTop() > 50){
         $(".header-panel-bg").css("opacity", "1");
         if (window.innerWidth > 500) {
@@ -49,41 +48,27 @@ $(window).scroll(function () {
             } else{
                 $(".dropdown-content").css("backgroundColor", '#fff');
             }
-            
+
         }
-         $('.ham-li').css("backgroundColor", '#000');
+        $('.ham-li').css("backgroundColor", '#000');
     } else
     {
         if (window.innerWidth > 500) {
-         $(".header-text").css("color", '#fff');
-        $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
-        if (window.innerWidth < 1100) {
+            $(".header-text").css("color", '#fff');
+            $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
+            if (window.innerWidth < 1100) {
                 $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
                 $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
             } else{
                 $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
             }
-        //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
+            //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
         }
-         $(".header-panel-bg").css("opacity", "0");
-          $('.ham-li').css("backgroundColor", '#fff');
+        $(".header-panel-bg").css("opacity", "0");
+        $('.ham-li').css("backgroundColor", '#fff');
     }
+};
 
-    /*
-    var offset = $(document).scrollTop()
-    var opacity = 0;
-    if (offset <= 0) {
-        opacity = 0;
-    } else if (offset > 0 & offset <= 200) {
-        opacity = (offset - 1) / 200;
-    }
-    else if (offset > $(document).height() - $(window).height() - 64){
-        opacity = 0;
-    }
-    else {
-        opacity = 1;
-    }
+$(window).load(scrollHeaderHandler);
 
-    $('.header-panel-bg').css('opacity', opacity);
-    */
-});
+$(window).scroll(scrollHeaderHandler);
