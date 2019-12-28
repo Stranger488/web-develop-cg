149220e6cc7e83dc666056474b431eeb09a8bf33
Ilya Goltsov <ilya_goltsov@list.ru>
2019-12-28 03:22:21 +0300

    menu underitems highlighting, advantages block on flex, correcting menu script

--- js/menu.js
+++ js/menu.js
@@ -13,7 +13,6 @@ $(document).ready(function () {
         $(".header-text").css("color", '#fff');
         $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
     } else {
-        //$('#menu').css("background-color", "rgba(255,255,255,1)");
         $(".header-text").css("color", '#000');
         $(".whiteBg").css("backgroundColor", 'rgba(255,255,255,1)');
     }
@@ -33,12 +32,11 @@ $(window).resize(function () {
     } else {
         $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0');
     }
+
     if (window.innerWidth > 500 && $(window).scrollTop() < 50) {
         $('#menu').css("background-color", "rgba(0,0,0,0)");
         $(".header-text").css("color", '#fff');
-        //$(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
     } else {
-        //$('#menu').css("background-color", "rgba(255,255,255,1)");
         $(".header-text").css("color", '#000');
         $(".whiteBg").css("backgroundColor", 'rgba(255,255,255,1)');
     }
@@ -56,29 +54,32 @@ $(window).resize(function () {
 const scrollHeaderHandler = () => {
     if ($(window).scrollTop() > 50) {
         $(".header-panel-bg").css("opacity", "1");
+
         if (window.innerWidth > 500) {
             $(".header-text").css("color", '#000');
+
             if (window.innerWidth < 1100) {
                 $(".whiteBg").css("backgroundColor", '#fff');
-                //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
             } else {
                 $(".dropdown-content").css("backgroundColor", '#fff');
             }
 
         }
+
         $('.ham-li').css("backgroundColor", '#000');
     } else {
         if (window.innerWidth > 500) {
             $(".header-text").css("color", '#fff');
             $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
+
             if (window.innerWidth < 1100) {
                 $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
                 $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
             } else {
                 $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
             }
-            //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
         }
+
         $(".header-panel-bg").css("opacity", "0");
         $('.ham-li').css("backgroundColor", '#fff');
     }
